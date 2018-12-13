ead734a0bd987e83e3b118c7fa7551bf67f5f471
Tanya Phung <tanya.n.phung@gmail.com>
2018-12-13 16:31:01 -0500

    add 2 panels

--- server.R
+++ server.R
@@ -1,12 +1,42 @@
 server = function(input, output) {
   
+  incident_rate = c(0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.8, 0.8, 0.8, 0.8, 0.8, 0.9, 0.9, 1.0, 1.1, 1.3, 1.6, 1.9, 2.5, 3.2, 4.2, 5.5, 7.0, 8.6, 10.3, 11.9, 13.4, 14.6, 15.6, 16.4, 17, 17.4)
+  incident_rate_scaled = incident_rate/10000
+  names(incident_rate_scaled) = c('16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49')
+  
+  specificity_vals = c(0.89, 0.88, 0.91)
+  names(specificity_vals) = c("Trisomy 13", "Trisomy 18", "Trisomy 21")
+  
+  panorama = c(0.99, 0.99, 0.99)
+  names(panorama) = c("Trisomy 13", "Trisomy 18", "Trisomy 21")
+  
   calc_true_pos_prob = function(specificity, incident) {
     prob_true_pos = (specificity*incident)/((specificity*incident) + (1-specificity)*(1-incident))
     return (prob_true_pos)
   }
   
-  output$prob_true_pos = renderText({
-    paste("The probability of a true positive test", calc_true_pos_prob(input$specificity, input$rate))
+  output$prob_true_pos1 = renderText({
+    perc = round(calc_true_pos_prob(input$specificity, input$rate) * 100, digits=2)
+    paste("The probability of a true positive test", perc, "%")
+  })
+
+  output$prob_true_pos2 = renderText({
+    
+    if (input$specificity_cat == "gil") {
+    
+    specificity = specificity_vals[input$name]
+    rate = incident_rate_scaled[toString(input$age)]
+    perc = round(calc_true_pos_prob(specificity, rate) * 100, digits = 2)
+    paste("The probability of a true positive test", perc, "%")
+    }
+    
+    else {
+      specificity = panorama[input$name]
+      rate = incident_rate_scaled[toString(input$age)]
+      perc = round(calc_true_pos_prob(specificity, rate) * 100, digits = 2)
+      paste("The probability of a true positive test", perc, "%")
+    }
+    
   })
   
 }
--- ui.R
+++ ui.R
@@ -4,14 +4,27 @@ library(shiny)
 ui <- fluidPage(
   
   # App title ----
-  titlePanel("Non Invasive Prenatal Testing analyses"),
+  titlePanel("Calculate the probability of a true positive in NIPT"),
   
-  # Copy the line below to make a number input box into the UI.
-  numericInput("specificity", label = h3("Enter the specificity of the test:"), 0.997),
-  numericInput("rate", label = h3("Enter the incident rate of the disease:"), 0.0001),
+  mainPanel(
+    tabsetPanel(
+      tabPanel("User-defined values", 
+               h4("If you know what the specificity of the test and the incident rate of the disease, enter those numbers here. Otherwise, click the tab: Predefined values"),
+               numericInput("specificity", label = h3("Enter the specificity of the test:"), 0.997),
+               numericInput("rate", label = h3("Enter the incident rate of the disease:"), 0.0001),
+               h3("Result:"),
+               textOutput("prob_true_pos1")),
+        
+      tabPanel("Predefined values",
+               h4("Here, we use the specificity for Trisomy 13, Trisomy 18, and Trisomy 21 from Gil et al. 2015. The incident rate is obtained from Savva et al. 2010. You just need to enter the name of the test and your age."),
+              textInput("name", label = h3("Enter the disease name:"), "Trisomy 13"),
+              numericInput("age", label = h3("Enter your age:"), 26, min=16, max=49),
+              selectInput("specificity_cat", h3("Select which values of specificity to calculate:"),
+                                 c("Gil et al. 2015" = "gil",
+                                   "Panorama" = "panorama"), selected = "gil"),
+              h3("Result:"),
+              textOutput("prob_true_pos2"))
+  )
   
-  hr(),
-  
-  fluidRow(column(3, textOutput("prob_true_pos")))
-  
+)
 )
