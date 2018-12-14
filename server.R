c11b35b49f8fb51be6176a818322ee73fb549a25
Tanya Phung <tanya.n.phung@gmail.com>
2018-12-14 15:34:17 -0500

    rewrote the function to calculate the probability

--- server.R
+++ server.R
@@ -1,33 +1,38 @@
 server = function(input, output) {
   
+  # Setting up some data for the predefined parts of the app.
+  
   incident_rate = c(0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.8, 0.8, 0.8, 0.8, 0.8, 0.9, 0.9, 1.0, 1.1, 1.3, 1.6, 1.9, 2.5, 3.2, 4.2, 5.5, 7.0, 8.6, 10.3, 11.9, 13.4, 14.6, 15.6, 16.4, 17, 17.4)
   incident_rate_scaled = incident_rate/10000
   names(incident_rate_scaled) = c('16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49')
   
-  specificity_vals = c(0.89, 0.88, 0.91)
-  names(specificity_vals) = c("Trisomy 13", "Trisomy 18", "Trisomy 21")
+  study1 = data.frame(disease=c("Trisomy 21", "Trisomy 18", "Trisomy 13"), true_pos = c(0.993, 0.974, 0.974), true_neg= c(0.999, 0.999, 0.999))
   
-  panorama = c(0.99, 0.99, 0.99)
-  names(panorama) = c("Trisomy 13", "Trisomy 18", "Trisomy 21")
+  # specificity_vals = c(0.9989, 0.9988, 0.9991)
+  # names(specificity_vals) = c("Trisomy 13", "Trisomy 18", "Trisomy 21")
+  # 
+  # panorama = c(0.99, 0.99, 0.99)
+  # names(panorama) = c("Trisomy 13", "Trisomy 18", "Trisomy 21")
   
-  calc_true_pos_prob = function(specificity, incident) {
-    prob_true_pos = (specificity*incident)/((specificity*incident) + (1-specificity)*(1-incident))
+  calc_true_pos_prob = function(true_pos, true_neg, incident) {
+    prob_true_pos = (true_pos*incident)/(true_pos*incident + (1-true_neg)*(1-incident))
     return (prob_true_pos)
   }
   
   output$prob_true_pos1 = renderText({
-    perc = round(calc_true_pos_prob(input$specificity, input$rate) * 100, digits=2)
-    paste("The probability of a true positive test", perc, "%")
+    perc = round(calc_true_pos_prob(input$true_pos, input$true_neg, input$rate) * 100, digits=2)
+    paste("The probability that you have the condition given that you tested positive", perc, "%")
   })
 
   output$prob_true_pos2 = renderText({
     
-    if (input$specificity_cat == "gil") {
-    
-    specificity = specificity_vals[input$name]
-    rate = incident_rate_scaled[toString(input$age)]
-    perc = round(calc_true_pos_prob(specificity, rate) * 100, digits = 2)
-    paste("The probability of a true positive test", perc, "%")
+    if (input$studies == "study1") {
+      
+      true_pos = subset(study1, study1$disease==input$name)[2]
+      true_neg = subset(study1, study1$disease==input$name)[3]
+      rate = incident_rate_scaled[toString(input$age)]
+    perc = round(calc_true_pos_prob(true_pos, true_neg, rate) * 100, digits=2)
+    paste("The probability that you have the condition given that you tested positive", perc, "%")
     }
     
     else {
--- ui.R
+++ ui.R
@@ -6,25 +6,26 @@ ui <- fluidPage(
   # App title ----
   titlePanel("Calculate the probability of a true positive in NIPT"),
   
-  mainPanel(
+  # mainPanel(
     tabsetPanel(
       tabPanel("User-defined values", 
-               h4("If you know what the specificity of the test and the incident rate of the disease, enter those numbers here. Otherwise, click the tab: Predefined values"),
-               numericInput("specificity", label = h3("Enter the specificity of the test:"), 0.997),
-               numericInput("rate", label = h3("Enter the incident rate of the disease:"), 0.0001),
+               h4("If you know the true positive rate and false positive rate of your test, and the incident rate of the disease, enter those numbers here. Otherwise, click on the tab Predefined values."),
+               numericInput("true_pos", label = h3("Enter the true positive rate of the test:"), 0.974),
+               numericInput("true_neg", label = h3("Enter the true negative rate of the test:"), 0.999),
+               numericInput("rate", label = h3("Enter the incident rate of the disease:"), 8e-5),
                h3("Result:"),
                textOutput("prob_true_pos1")),
         
       tabPanel("Predefined values",
-               h4("Here, we use the specificity for Trisomy 13, Trisomy 18, and Trisomy 21 from Gil et al. 2015. The incident rate is obtained from Savva et al. 2010. You just need to enter the name of the test and your age."),
+               h4("Here, the incident rate is obtained from Savva et al. 2010. You can select which studies to use the values for the true positive rate and the true negative rate. You just need to select the disease and your age."),
               textInput("name", label = h3("Enter the disease name:"), "Trisomy 13"),
               numericInput("age", label = h3("Enter your age:"), 26, min=16, max=49),
-              selectInput("specificity_cat", h3("Select which values of specificity to calculate:"),
-                                 c("Gil et al. 2015" = "gil",
-                                   "Panorama" = "panorama"), selected = "gil"),
+              selectInput("studies", h3("Select which studies to obtain values for the true positive rate and the true negative rate:"),
+                                 c("Taylor-Phillips et al. 2015" = "study1",
+                                   "Panorama" = "panorama"), selected = "study1"),
               h3("Result:"),
               textOutput("prob_true_pos2"))
   )
   
 )
-)
+# )
